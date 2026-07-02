a7db4731b173a5e47a8a17cc47658937597a2419
TridevSharma1 <tridevx8@gmail.com>
2026-07-03 00:57:48 +0530

    Revert "added"
    This reverts commit b5cc54e916c9df80856a6c1562913d03356c56cd.

--- divorce_connect/fastapi_app/broker.py
+++ divorce_connect/fastapi_app/broker.py
@@ -17,4 +17,4 @@ broker = NatsBroker(
 # You can add middlewares here (e.g., for logging, retry logic, or result backends)
 
 # THIS LINE IS MANDATORY: It forces the broker to discover and register your tasks!
-import divorce_connect.fastapi_app.tasks
+import fastapi_app.tasks
